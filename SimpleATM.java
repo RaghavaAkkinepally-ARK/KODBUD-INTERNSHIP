9a47366162aeea6b39879d841b432d7683ddad31
Raghava Akkinepally <219287920+RaghavaAkkinepally-ARK@users.noreply.github.com>
2025-12-29 18:25:46 +0530

    Add files via upload

---
+++ SimpleATM.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class SimpleATM {
+
+    static double balance = 1000.0;  
+
+    static void checkBalance() {
+        System.out.println("Current Balance: ₹" + balance);
+    }
+
+    
+    static void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Amount deposited successfully.");
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    static void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient balance.");
+        } else if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount.");
+        } else {
+            balance -= amount;
+            System.out.println("Please collect your cash.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n=== ATM MENU ===");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    checkBalance();
+                    break;
+
+                case 2:
+                    System.out.print("Enter amount to deposit: ");
+                    double depositAmount = sc.nextDouble();
+                    deposit(depositAmount);
+                    break;
+
+                case 3:
+                    System.out.print("Enter amount to withdraw: ");
+                    double withdrawAmount = sc.nextDouble();
+                    withdraw(withdrawAmount);
+                    break;
+
+                case 4:
+                    System.out.println("Thank you for using the ATM.");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
